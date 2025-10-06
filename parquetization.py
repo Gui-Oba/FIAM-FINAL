d12069623dde434fc8307bf66bd931d14f82a7bd
Gui <149915655+Gui-Oba@users.noreply.github.com>
2025-10-05 23:15:15 -0400

    Add files via upload

---
+++ parquetization.py
@@ -0,0 +1,11 @@
+import duckdb
+
+duckdb.sql("""
+COPY (
+  SELECT *,
+         year(STRPTIME(CAST(date AS VARCHAR), '%Y%m%d'))  AS y,
+         month(STRPTIME(CAST(date AS VARCHAR), '%Y%m%d')) AS m
+  FROM '../data/ret_sample.csv'
+) TO 'ret_parquet'
+  (FORMAT PARQUET, PARTITION_BY (y,m), COMPRESSION ZSTD);
+""")
